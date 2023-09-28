e01c542901a4e85682f80a61306958c0b34dbea5
Nahid345 <nahidparvej07@gmail.com>
2023-09-29 03:33:19 +0600

    hamming distance

---
+++ hammingDistance.js
@@ -0,0 +1,29 @@
+// hamming Distance
+
+//input : hello, hrlle
+// output: 2
+
+function hammingDistance(x, y) {
+    x = x.toString(2);
+    y = y.toString(2)
+
+    if (x.length < y.length) {
+
+         while(x.length !== y.length) x = "0" + x;
+    }
+    else{
+        while(x.length !== y.length) y = "0" + y; 
+    }
+
+    let distance = 0;
+
+    for (let i = 0; i < x.length; i++) {
+        if (x[i] !== y[i]) {
+            distance++;
+        }
+    }
+
+    return distance;
+}
+
+console.log(hammingDistance("hello", "hrlle"))
